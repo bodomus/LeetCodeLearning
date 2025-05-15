7a6777fa73a10186f7705a0bf5ff565fe9fb08f3
Андрей Андрей <bodomus@gmail.com>
2025-05-15 19:36:11 +0300

    First commit

---
+++ Lessons/1550/LeetCodeConsoleApp-1550/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+var r = new Solution();
+int[] arr = [1, 2, 34, 3, 4, 5, 7, 23, 12];
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine($" 3 time ConsecutiveOdds exist {r.ThreeConsecutiveOdds(arr)}");
+
+
+public class Solution
+{
+    public bool ThreeConsecutiveOdds(int[] data)
+    {
+        var ar = data.Select((value, index) => new { Index = index, Value = value }).ToArray();
+
+        foreach (var item in ar)
+        {
+            Console.WriteLine($"Индекс: {item.Index}, Значение: {item.Value}");
+        }
+
+        int total = data.Count();
+        for (int i = 0; i < total - 2; i++)
+        {
+            if (((ar[i].Value & 0x1) == 1) && ((ar[i + 1].Value & 0x1) == 1) && ((ar[i + 2].Value & 0x1) == 1)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Lessons/1550/TestProjectThreeConsecutiveOdds-1550/TestProjectThreeConsecutiveOdds.cs
@@ -0,0 +1,21 @@
+﻿using AutoFixture;
+
+namespace TestProjectThreeConsecutiveOdds;
+
+public class TestProjectThreeConsecutiveOdds
+{
+    private readonly Fixture _fixture = new Fixture();
+    private Solution test = new Solution(); 
+    
+    [Fact]
+    public void ThreeConsecutiveOdds()
+    {
+        int[] invalidArray = new int[10]{1,2,3,4,5,6,7,8,9,10};
+        int[] validArray = new int[8]{1,3,5,6,7,8,9,10};
+        int[] validArray1 = new int[7]{1,2,3,6,7,9,11};
+
+        Assert.False(test.ThreeConsecutiveOdds(invalidArray));
+        Assert.True(test.ThreeConsecutiveOdds(validArray));
+        Assert.True(test.ThreeConsecutiveOdds(validArray1));
+    }
+}
