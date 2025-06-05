eb81a76299d880939c9afc97766613ef16d1d761
Андрей Андрей <bodomus@gmail.com>
2025-06-05 20:00:44 +0300

    dev. + lesson 1.

---
+++ Lessons/1/LeetCodeConsoleApp-1/MySolution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeConsoleApp_1;
+
+public class MySolution
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var lookup = new Dictionary<int, int>(capacity: nums.Length);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int need = target - nums[i];
+
+            if (lookup.TryGetValue(need, out int j))
+                return new[] { j, i };           // нашли пару
+
+            // сохраняем текущий элемент, если такую пару ещё не видели
+            lookup[nums[i]] = i;
+        }
+
+        return Array.Empty<int>();               // или throw new InvalidOperationException(...)
+    }
+}
---
+++ Lessons/1/TestProjectTwoSum-1/TestProjectTwoSum-1.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeConsoleApp_1; 
+public class TestProjectTwoSum
+{
+    [Fact]
+    public void Test1()
+    {
+        var m = new MySolution();
+        int[] invalidArray = new int[10]{1,2,3,4,5,6,7,8,9,10};
+        int invalid1 = 124;
+        var rr = MySolution.TwoSum(invalidArray, invalid1);
+        Assert.True(rr.Length == 0);
+//
+
+        int[] validArray = new int[8]{1,3,5,6,7,8,9,10};
+        int valid2 = 9;
+        rr = MySolution.TwoSum(validArray, valid2);
+        int[] valid = new int [] {1, 3};
+        Assert.Equal(rr, valid);
+//
+
+        int[] validArray1 = new int[7]{1,2,3,6,7,9,11};
+        int valid3 = 12;
+        rr = MySolution.TwoSum(validArray1, valid3);
+        valid = new int [] {0, 6};
+        Assert.Equal(rr, valid);
+        
+        int[] validArray2 = new int[7]{1,2,3,6,7,9,11};
+        int valid4 = 12;
+        rr = MySolution.TwoSum(validArray2, valid4);
+        valid = new int [] {0, 6};
+        Assert.Equal(rr, valid);
+    }
+}
