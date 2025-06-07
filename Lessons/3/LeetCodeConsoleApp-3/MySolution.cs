65d4bc7ae177ebd253d200efcce6b61e3efdaf4d
Андрей Андрей <bodomus@gmail.com>
2025-06-07 11:47:48 +0300

    Dev + Lesson - 3

---
+++ Lessons/3/LeetCodeConsoleApp-3/MySolution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeConsoleApp_3;
+
+public class MySolution
+{
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        //nums1.Concat(nums2).ToList().Sort();
+        int[] merged1 = nums1.Concat(nums2).ToArray();
+        var merged = merged1.Order().ToList();
+        int l = merged.Count;
+        double result = 0;
+        
+        if (l % 2 == 0)
+        {
+            result = (merged[l / 2 - 1] + merged[l / 2]) / 2.0;
+        }
+        else
+        {
+            
+            result = (merged[l / 2]);
+        }
+        return result;
+    }
+}
---
+++ Lessons/3/TestProjectFindMedianSortedArrays-3/TestFindMedianSortedArrays.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeConsoleApp_3;
+
+public class TestFindMedianSortedArrays
+{
+    [Fact]
+    public void Test1()
+    {
+        var mySolution = new MySolution();
+        int[] a1 = [1, 3];
+        int[] a2 = [2];
+
+        var d = mySolution.FindMedianSortedArrays(a1, a2);
+        Assert.Equal(2.00000, d);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        var mySolution = new MySolution();
+        int[] a1 = [1, 2];
+        int[] a2 = [3, 4];
+
+        var d = mySolution.FindMedianSortedArrays(a1, a2);
+        Assert.Equal(2.50000, d);
+    }
+    
+    
+}
